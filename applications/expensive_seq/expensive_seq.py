b7cac9fa3386c4bf7d73edc1aa5d21aa671f04fc
Vanessa <vanessatellez45@gmail.com>
2020-05-07 20:01:00 -0400

    expensive_seq done, markov WIP

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,22 +1,19 @@
 cache = {}
 
 
-def exps(x, y, z):
-    while x >= 0 and y >= 0 and z >= 0:
-        if x <= 0:
-            return y + z
-        if x > 0:
-            return expensive_seq(x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
-
-
 def expensive_seq(x, y, z):
     # Check to see if (x, y, z) is in the cache
-    if (x, y, z) not in cache:
-        # If not, store (x,y,z) as key, run function, and store result as the value
-        cache[(x, y, z)] = exps(x, y, z)
+    if x <= 0:
+        return y + z
+    if x > 0:        
+        if (x, y, z) not in cache:
+            # If not, store (x,y,z) as key, run function, and store result as the value
+            cache[(x, y, z)] = expensive_seq(x-1, y+1, z) + \
+                            expensive_seq(x-2, y+2, z*2)+ \
+                            expensive_seq(x-3, y+3, z*3)
 
     # If it is, returns value
-    return cache[(x, y, z)]
+        return cache[(x, y, z)]
 
 
 if __name__ == "__main__":
--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,9 +1,18 @@
 import random
 
+wordbank = {}
+
 # Read in all the words in one go
 with open("input.txt") as f:
     words = f.read()
 
 # TODO: analyze which words can follow other words
+list_of_words = words.split()
 
+for i in range(len(list_of_words) - 1):
+    if list_of_words[i] not in wordbank:
+        wordbank[list_of_words[i]].append(list_of_words[i+1])
+
+
+print(wordbank)
 # TODO: construct 5 random sentences
